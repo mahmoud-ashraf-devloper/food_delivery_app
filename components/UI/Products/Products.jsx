f82026e3975e6384f19e75a66c5bc5ebd6419eb2
Mahmoud Ashraf <88296664+mahmoud-ashraf-devloper@users.noreply.github.com>
2022-07-08 17:15:50 +0200

    fixing filter bug

--- components/UI/Products/Products.jsx
+++ components/UI/Products/Products.jsx
@@ -3,30 +3,30 @@ import Burger from '../../../public/assets/images/hamburger.png'
 import Bread from '../../../public/assets/images/bread.png'
 import Pizza from '../../../public/assets/images/pizza.png'
 import Image from 'next/image';
-import AllProductsData  from '../../../public/assets/fake-data/products.js';
+import AllProductsData from '../../../public/assets/fake-data/products.js';
 import Product from './Product';
 const AllFilters = [
     {
+        id: 0,
         name: 'All',
         keyword: 'all',
-        isActive: true,
     },
     {
+        id: 1,
         name: 'Pizza',
         keyword: 'pizza',
-        isActive: false,
         image: Pizza,
     },
     {
+        id: 2,
         name: 'burger',
         keyword: 'burger',
-        isActive: false,
         image: Burger,
     },
     {
+        id: 3,
         name: 'Bread',
         keyword: 'bread',
-        isActive: false,
         image: Bread,
     },
 ]
@@ -34,37 +34,32 @@ const Products = () => {
 
     const [allProducts, setAllProducts] = useState(AllProductsData);
     const [filters, setFilters] = useState(AllFilters)
+    const [currentFilter, setCurrentFilter] = useState(AllFilters[0])
 
-    const doFilter = (keyword) => {
-        if (keyword === 'all') {
+    const doFilter = (filter) => {
+        setCurrentFilter(filter)
+        if (filter.keyword === 'all') {
             setAllProducts(AllProductsData)
         } else {
-            const filtered = AllProductsData.filter((product) => product.category.toLowerCase() === keyword)
-            filters.forEach((filter, index) => {
-                if (filter.keyword === keyword) {
-                    filters[index].isActive = true
-                }
-                filters[index].isActive = false
-            })
+            const filtered = AllProductsData.filter((product) => (product.category.toLowerCase() === filter.keyword))
             setAllProducts(filtered)
         }
     }
-
     return (
         <div className='max-w-[1240px] mx-auto min-h-screen'>
             <h1 className="text-center text-3xl font-bold uppercase tracking-widest py-8">Popular Food</h1>
             <div className="my-3 mx-3 px-2 flex justify-center items-center h-16 rounded-xl text-white bg-red-500">
                 {filters.map((filter, index) =>
-                    <div onClick={() => { doFilter(filter.keyword); filter.isActive = true }} key={index} className={filter.isActive ? "font-bold cursor-pointer flex justify-center items-center mx-4 bg-white text-red-600 px-3 py-1 rounded-lg text-center" : "font-bold cursor-pointer flex justify-center items-center mx-4"}>
-
+                    <div onClick={() => { doFilter(filter)}} key={index} className={filter.id === currentFilter.id? "font-bold cursor-pointer flex justify-center items-center mx-4 bg-white text-red-600 px-3 py-1 rounded-lg text-center" : "font-bold cursor-pointer flex justify-center items-center mx-4"}>
+    
                         {(index === 0) ? '' : <span className='w-8 h-8 mx-2 hidden md:flex'><Image src={filter.image} alt='/' /></span>}
-
+    
                         {filter.name}
                     </div>
                 )}
             </div>
             <div className='grid md:grid-cols-3 gap-6 lg:grid-cols-4 my-6 mx-3'>
-
+    
                 {
                     allProducts.map((item, index) =>
                         <Product key={index} item={item} />
@@ -73,6 +68,8 @@ const Products = () => {
             </div>
         </div>
     );
+    
 }
+
 
 export default Products;
