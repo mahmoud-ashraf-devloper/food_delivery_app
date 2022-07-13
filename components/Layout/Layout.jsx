4482e88318ccfca155d54af5637ad39b22dfeb22
Mahmoud Ashraf <88296664+mahmoud-ashraf-devloper@users.noreply.github.com>
2022-07-13 18:09:47 +0200

    done

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import { Head } from 'next/head';
+import  Head  from 'next/head';
 
 const Layout = ({ children }) => {
     return (
